d5632c0d30feea760695ee8498eed629d523b1fc
Ammon Lauritzen <allaryin@gmail.com>
2015-07-01 10:27:09 -0500

    Updated to use jessie and znc 1.6.0

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,11 @@
-# version 1.4-1
-# docker-version 0.11.1
-FROM        ubuntu:12.04
-MAINTAINER  Jim Myhrberg "contact@jimeh.me"
+FROM        debian:jessie
+MAINTAINER  Ammon Lauritzen "allaryin@gmail.com"
 
 # We use a bootstrap script to avoid having temporary cache files and build
 # dependencies being committed and included into the docker image.
 ADD         bootstrap.sh /tmp/
-RUN         chmod +x /tmp/bootstrap.sh && /tmp/bootstrap.sh
+RUN         chmod +x /tmp/bootstrap.sh
+RUN         /tmp/bootstrap.sh
 
 RUN         useradd znc
 ADD         start-znc /usr/local/bin/
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,8 @@
 #! /usr/bin/env bash
 set -e
 
-
 # Config
-ZNC_VERSION="1.4"
-
+ZNC_VERSION="1.6.0"
 
 # Ensure package list is up to date.
 apt-get update
@@ -15,10 +13,8 @@ apt-get install -y sudo
 # Install build dependencies.
 apt-get install -y wget build-essential libssl-dev libperl-dev pkg-config
 
-
 # Prepare building
 mkdir -p /src
-
 
 # Download, compile and install ZNC.
 cd /src
@@ -26,7 +22,6 @@ wget "http://znc.in/releases/archive/znc-${ZNC_VERSION}.tar.gz"
 tar -zxf "znc-${ZNC_VERSION}.tar.gz"
 cd "znc-${ZNC_VERSION}"
 ./configure && make && make install
-
 
 # Clean up
 apt-get remove -y wget
